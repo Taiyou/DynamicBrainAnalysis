be1ebf3ff95dc1f921bee29805467a61c60103fc
HiroHamada <gutenmorgen1210@gmail.com>
2018-10-25 01:15:28 +0900

    Update myfunc_returnMap.m

--- myfunc_returnMap.m
+++ myfunc_returnMap.m
@@ -1,9 +1,12 @@
-function [] = myfunc_returnMap(data, D)
+function [] = myfunc_returnMap(data, D,option)
 % This program is intended to visualize the return map of the time series data
 % 
 % input:
 % 	date: time series data (timeseries x 1)
 %	D: maximum delay for the return Map 
+%	option: linear or polar coordinates
+%		'linear':
+%		'polar':
 %
 % output:
 %	visualization of the datasets
@@ -16,11 +19,24 @@ for delay = 1:D
 	X = data(1:length(data(1,:))-delay+1);
 	X_delay = data(delay:length(data(1,:)));
 
-	plot(X,X_delay)
 
+	if option = 'linear',
+		plot(X,X_delay)
+
+	else if option = 'polar',
+		% theta
+		theta_n = atan(X_delay./X)
+		theta_n1 = theta_n(1:length(theta_n)-1)
+		theta_n2 = theta_n(2:length(theta_n))
+
+		plot(theta_n1, theta_n2,'.')
+
+	% default
+	else
+		plot(X,X_delay)
+	end
+	
 	title('ReturnMap')
 	xlabel('X(t)');
 	ylabel(strcat('X(t+', num2str(delay), ')'))
 end
-
-
